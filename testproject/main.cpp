3f3be1befb2eb78472b0a7f4e584472aed55bab2
DmitryBagaev <dr.chebys@yandex.ru>
2014-11-06 12:38:19 +0400

    using set

--- testproject/main.cpp
+++ testproject/main.cpp
@@ -1,61 +1,54 @@
 #include <iostream>
 #include <cstring>
-#include <vector>
+#include <set>
 
 using namespace std;
 
-string qLetterType (string s);
+string qLetterType (char c);
 
 int main ()
 {
     string s;
-    string vowel = "vowel";
-    string consonant = "consonant";
 
-    cout << "Insert letter ";
+    cout << "Insert letter: ";
     cin  >> s;
-    cout << qLetterType(s);
-
+    if ( s.length() != 1 ){
+        cout << "Invalid number of symbols\n";
+    } else {
+        char c = s[0];
+        cout << qLetterType(c);
+    }
     return 0;
 }
 
-string qLetterType (string s){
+string qLetterType (char c){
+
+    string stringOut;
     string lettersVowel = "aeiouy";
     string lettersConsonant = "bcdfghjklmnpqrstvwxz";
     int lengthVowel = lettersVowel.length();
     int lengthConsonant = lettersConsonant.length();
 
-    vector <char> letters;
-    string stringOut;
-
-    for (int i=0; i<lengthVowel; i++){
-        letters.push_back(lettersVowel[i]);
+    set <char> vowels;
+    set <char> consonants;
+    set <char>::iterator it;
+    for (int i=0; i<lengthVowel;i++){
+        vowels.insert(lettersVowel[i]);
     }
-
-    for (int i=0; i<lengthConsonant; i++){
-        letters.push_back(lettersConsonant[i]);
+    for (int i=0; i<lengthConsonant;i++){
+        consonants.insert(lettersConsonant[i]);
     }
-
-    int lengthVector = letters.size();
-    int par = 1;
-
-    if (s.length()==1){
-        for (int i=0; i<lengthVector; i++){
-            if (s[0]==letters[i]){
-                par = 0;
-                if ( i < lengthVowel-1 ){
-                    stringOut = "Vowel\n";
-                } else {
-                    stringOut = "Consonant\n";
-                }
-            }
+    it = vowels.find(c);
+    if ( it == vowels.end() ){
+        it = consonants.find(c);
+        if (it == consonants.end() ){
+            stringOut = "Invalid symbol: not a letter\n";
+        }else{
+            stringOut = "Consonant\n";
         }
-        if ( par == 1){
-         stringOut = "Error: not a letter\n";
-        }
-    } else {
-        stringOut = "Error: not a single char\n";
-
+    }else{
+        stringOut = "Vowel\n";
     }
+
     return stringOut;
 }
