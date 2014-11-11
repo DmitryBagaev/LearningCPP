93204eb6cded269d4db4538dd097385b93fd1f08
DmitryBagaev <dr.chebys@yandex.ru>
2014-11-11 18:12:03 +0400

    New version of array task -- with templates

--- basicsproject/main.cpp
+++ basicsproject/main.cpp
@@ -2,22 +2,26 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include "secondtask.cpp"
 
 using namespace std;
 
-int oneDimention();
-int twoDimentionsWithPointers();
-int trainStructure();
+void oneDimension(int size);
+void twoDimensionsWithPointers(int size);
+void trainStructure(int trainAmount);
+template <class T> void betweenAB(T arr, int size, int begin, int end);
+template <class T> void afterMax(T arr, int size);
+template <class T> void sort (T arr, int size);
 
 int main()
 {
-    //    oneDimention();
-    //twoDimentionsWithPointers();
-    trainStructure();
+    oneDimension(15);
+    //twoDimensionsWithPointers(10);
+    //trainStructure(6);
     return 0;
 }
 
-int trainStructure()
+void trainStructure(int trainAmount)
 {
     struct Train{
         string direction;
@@ -25,42 +29,38 @@ int trainStructure()
         string time;
     };
 
-    int trainAmount = 6;
     Train trainArr[trainAmount];
 
     for (int i = 0; i < trainAmount; i++)
     {
-        cout << endl << "Введите пункт назначения " << i << "-ого поезда: ";
+        cout << endl << "Input direction of " << i << "train: ";
         cin >> trainArr[i].direction;
-        cout << endl << "Введите его номер: ";
+        cout << endl << "Input it's number: ";
         cin >> trainArr[i].number;
-        cout << endl << "Введите час отправления: ";
+        cout << endl << "Input hour of departure: ";
         cin >> trainArr[i].time;
     }
 
     int trainCounter = 0;
     string trainDirection;
-    cout << endl << "Введите искомый пункт назначения:";
+    cout << endl << "Input your direction:";
     cin >> trainDirection;
-     for (int i = 0; i < trainAmount; i++)
-         if (trainDirection == trainArr[i].direction){
-             trainCounter++;
-            cout << endl << "Пункт назначения: " << trainArr[i].direction << endl
-                    << "Номер: " << trainArr[i].number << endl
-                       << "Час отправления: " << trainArr[i].time << endl << endl;
-         }
-    if (trainCounter == 0)
-        cout << endl << "Таких поездов нет" << endl;
-
-    return 0;
+    for (int i = 0; i < trainAmount; i++)
+        if (trainDirection == trainArr[i].direction){
+            trainCounter++;
+            cout << endl << "Direction: " << trainArr[i].direction << endl
+                 << "Number: " << trainArr[i].number << endl
+                 << "Hour of departure: " << trainArr[i].time << endl;
+        }
+    if (!trainCounter)
+        cout << endl << "There's no train with this direction" << endl;
 }
 
 
-int twoDimentionsWithPointers()
+void twoDimensionsWithPointers(int size)
 {
-        srand(time(NULL));
+    srand(time(nullptr));
 
-    int size = 10;
     int **arr = new int *[size + 2];
     for (int i = 0; i < size + 2; i++)
         arr[i] = new int [size + 2];
@@ -73,7 +73,7 @@ int twoDimentionsWithPointers()
         }
     }
 
-    cout << "Массив:" << endl;
+    cout << "Array:" << endl;
 
     for (int i = 1; i < size+1; i++)
     {
@@ -89,14 +89,14 @@ int twoDimentionsWithPointers()
     for (int i = 1; i < size + 1; i++)
     {
         for (int j = 1; j < size + 1; j++){
-            if ((arr[i][j] < arr[i][j+1]) & (arr[i][j] < arr[i][j-1]) & (arr[i][j] < arr[i+1][j]) & (arr[i][j] < arr[i-1][j]))
+            if ((arr[i][j] < arr[i][j+1]) && (arr[i][j] < arr[i][j-1]) && (arr[i][j] < arr[i+1][j]) && (arr[i][j] < arr[i-1][j]))
             {
                 counter += 1 ;
             }
         }
     }
 
-    cout << "Число \"слабых\" элементов: " << counter << endl;
+    cout << "Amount of \"weak\" elements: " << counter << endl;
 
     int sum = 0;
     for (int i = 1; i < size + 1; i++)
@@ -106,35 +106,55 @@ int twoDimentionsWithPointers()
                 sum += arr[i][j];
         }
     }
-    cout << "Сумма элементов выше главной диагонали: " << sum << endl;
-
-    return 0;
+    cout << "Sum of elements above the main diagonal: " << sum << endl;
+    delete[] arr;
 }
 
-int oneDimention()
+void oneDimension(int size)
 {
-    int size = 15;
     cout << "Size of array is : " << size << endl;
 
-    srand(time(NULL));
+    srand(time(nullptr));
 
-    double arr[size];
+    double arrDouble[size];
+    float arrFloat[size];
+    int arrInt[size];
 
     for (int i = 0; i < size; i++)
-        arr[i] = 0.02 * ( rand() % 10000 )- 100;
-    cout << "Массив случайных элементов от 1 до 100:" << endl;
+        arrDouble[i] = 0.02 * ( rand() % 10000 )- 100;
+    //cout << "Array of random elements from 1 to 100:" << endl;
+    //for (int i = 0; i < size; i++)
+    //  cout << arr[i] << " ";
+    //cout << endl;
+
     for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+        arrFloat[i] = 0.02 * ( rand() % 10000 )- 100;
+
+    for (int i = 0; i < size; i++)
+        arrInt[i] = 2 * rand() % 100 - 100;
 
     int a = 5;
     int b = 10;
 
-    cout << "Вывод элементов между " << a << " и " << b << endl;
-    for (int i = a; i < b-1; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+    betweenAB(arrDouble, size, a, b);
+    afterMax(arrFloat, size);
+    sort (arrInt, size);
+}
 
+template <class T> void betweenAB(T arr, int size, int begin, int end)
+{
+    cout << "Elements between " << begin << " and " << end << endl;
+    if ((begin >=0)&&(end<=size))
+        for (int i = begin; i < end-1; i++)
+            cout << arr[i] << " ";
+    else
+        cout << "Wrong parametrs of between function";
+    cout << endl;
+}
+
+
+template <class T> void afterMax(T arr, int size)
+{
     double max = arr[0];
     int maxPos = 0;
     for (int i = 0; i < size; i++)
@@ -147,17 +167,20 @@ int oneDimention()
     }
 
     double sum = 0;
-    for (int i = maxPos+1; i < size; i++) sum += arr[i];
-    cout << "Сумма элементов после максимального равна:" << sum << endl;
+    for (int i = maxPos+1; i < size; i++)
+        sum += arr[i];
+    cout << "Sum of the elements after max:" << sum << endl;
+}
 
+template <class T> void sort (T arr, int size)
+{
     for (int i = 0; i < size; ++i)
         for (int j = i+1; j < size ; ++j)
-            if ( fabs(arr[j]) > fabs(arr[i]) ) swap( arr[i], arr[j] );
+            if ( fabs(arr[j]) > fabs(arr[i]) )
+                swap( arr[i], arr[j] );
 
-    cout << "Отсортированный по убыванию массив:" << endl;
+    cout << "Sorted abs-s in descending order:" << endl;
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
-
-    return 0;
 }
